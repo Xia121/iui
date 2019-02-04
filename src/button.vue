68c7e454d364553167e3d08a0492f62adc1c99da
Xia121 <1751650135@qq.com>
2019-02-04 11:20:41 +0800

    npm install parcel

---
+++ src/button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        name: '',
+    }
+</script>
+
+<style lang="scss">
+.g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--button-radius);
+    border:1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+        border-color: var(--border-color-hover);
+    }
+    &:active {
+        border-color: var(--button-active-bg);
+    }
+}
+</style>
