8b2e051f88ea35888380b4d1a03a262315dc60d4
Xia121 <1751650135@qq.com>
2019-02-08 09:36:28 +0800

    验证iconPosition为left或right

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -11453,7 +11453,24 @@ exports.default = void 0;
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
 var _default = {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function validator(value) {
+        return value === 'left' || value === 'right';
+      }
+    }
+  },
   name: ''
 };
 exports.default = _default;
@@ -11469,7 +11486,23 @@ exports.default = _default;
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "g-button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "g-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
+  var _obj
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -11513,6 +11546,8 @@ var _button = _interopRequireDefault(require("./button"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+console.log(_button.default);
+
 _vue.default.component('g-button', _button.default);
 
 new _vue.default({
@@ -11548,7 +11583,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51138" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49556" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/button.vue
+++ src/button.vue
@@ -1,26 +1,74 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator (value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        },
         name: '',
     }
 </script>
 
 <style lang="scss">
-.g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--button-radius);
-    border:1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover {
-        border-color: var(--border-color-hover);
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--button-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-content: center;
+        vertical-align: middle;
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            border-color: var(--button-active-b g);
+        }
+        >.icon {
+            order: 1;
+            margin-right: .3em;
+        }
+        >.content {
+            order: 2;
+        }
+        &.icon-right {
+            >.icon {
+                order: 2;
+                margin-left: .3em;
+                margin-right: 0;
+            }
+            >.content {
+                order: 1;
+            }
+        }
+        &.icon-left {
+            >.icon {
+                order: 1;
+                margin-left: 0;
+                margin-right: .3em;
+            }
+            >.content {
+                order: 2;
+            }
+        }
     }
-    &:active {
-        border-color: var(--button-active-bg);
-    }
-}
 </style>
