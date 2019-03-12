faea21df9f3a4faf7488909126c757fb87a85c4b
Xia121 <1751650135@qq.com>
2019-03-12 15:52:36 +0800

    更改README.md

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-<h2>Iui - 基于Vue的UI框架</h2><br />
-<br />
-作者：夏晗文
-<br />
-<br />
-使用本框架前，请在css中开启border-box
+# IUI - 一个Vue UI 组件
+[![Build Status](https://travis-ci.org/travis-ci/docs-travis-ci-com.svg?branch=master)](https://travis-ci.org/travis-ci/docs-travis-ci-com)docs-travis-ci-com
+## 介绍
+
+## 开始使用
+
+1. 安装
+
+使用本框架前， 请在 CSS 中开启 border-box
+
+* {box-sizing: border-box;}
+
+
+## 文档
+
+## 提问
+
+## 变更记录
+
+## 联系方式 
+
+## 贡献代码
--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ new Vue({
 })
 
 import chai from 'chai'
+import spies from 'chai-spies'
+
+chai.use(spies)
+
 const expect = chai.expect
 //单元测试
 {
@@ -73,12 +77,11 @@ const expect = chai.expect
         }
     })
     vm.$mount()
-    vm.$on('click', function () {
-        console.log(1)
-    })
+    let spy = chai.spy(function () {})
+    vm.$on('click', spy)
     let button = vm.$el
     button.click()
-
+    expect(spy).to.have.been.called()
 }
 
 
